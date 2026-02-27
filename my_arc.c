e1f7ab7f2d764b5cd197744b7b8d97041b83d16a
Bohdan <oleoleqaz@gmail.com>
2026-02-27 21:28:07 +0200

    refractor code, add new functions

--- my_arc.c
+++ my_arc.c
@@ -3,32 +3,44 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <inttypes.h>
 
 #define FILE_PATH_BUFFER 4096
 #define FILE_NAME_BUFFER 255
 
 typedef struct 
 {
-    char filename[FILE_NAME_BUFFER];
     char filepath[FILE_PATH_BUFFER];
-
-    uint32_t file_size;
+    char filename[FILE_NAME_BUFFER];
+    
+    off_t file_size;
 }File_data;
 
 
-static int is_file(const char * filename)
+static int is_file(const char *filename)
 {
     struct stat st;
 
-    if (stat(filename,&st) == 0)
+    if (stat(filename, &st) == 0)
     {
         if (S_ISREG(st.st_mode))
+        {
             return 1;
+        }
+        else
+        {
+            printf("object %s is not a file\n", filename);
+            return 0;
+        }
     }
-    return 0;
+    else
+    {
+        printf("object %s doesn't exist\n", filename);
+        return 0;
+    }
 }
 
-static int extract_file_name(const char *filepath, char *filename) 
+static void extract_filename(char * filepath, char * filename)
 {
     const char *slash = strrchr(filepath, '/');
 
@@ -37,49 +49,82 @@ static int extract_file_name(const char *filepath, char *filename)
     else
         strcpy(filename, filepath);
 
-    if (!is_file(filepath))  
+}
+
+static void get_file_size(const char * filepath, off_t * file_size)
+{
+    struct stat st;
+    stat(filepath,&st);
+    *file_size = st.st_size;
+}
+       
+
+static inline void free_files(File_data **files)
+{
+    if (files && *files)
     {
-        printf("Object %s is not a file\n", filepath);
-        return 0;
+        free(*files);
+        *files = NULL;
+    }
+}
+
+static inline int increase_files(File_data **files, int *files_size, int *capacity)
+{
+    if (*files_size >= *capacity)
+    {
+        *capacity *= 2;
+
+        File_data *tmp = realloc(*files, sizeof(File_data) * (*capacity));
+        if (!tmp)
+            return 0;
+
+        *files = tmp;
     }
 
     return 1;
 }
+
 
 static int parse_files_data(File_data **files, int *files_size, char ** argv, int argc)
 {
     File_data * tmp = malloc(sizeof(File_data));
     if (!tmp)
         return 0;
-    *files_size = 0;
+
+    int size     = 0;
     int capacity = 1;
 
     for (int i = 2; i < argc; i++)
     {
-        if (capacity <= *files_size)
-        {
-            capacity *= 2;
-            tmp = realloc(tmp, sizeof(File_data ) * 2);
-            if (!tmp)
-                return 0;
-        }
+        if (!increase_files(&tmp, &size, &capacity))
+            return 0;
 
-        char filepath[FILE_PATH_BUFFER];        
+        char filepath[FILE_PATH_BUFFER];
         char filename[FILE_NAME_BUFFER];
+        off_t file_size = 0;
         
         strcpy(filepath, argv[i]);
-        if (!extract_file_name(filepath, filename))
-        {
+
+        if (!is_file(filepath)) {
+            free(tmp);
             return 0;
         }
         
-        *files_size++;
+        extract_filename(filepath,filename);
+        get_file_size(filepath, &file_size);
+
+
+        strcpy(tmp[size].filepath, filepath);
+        strcpy(tmp[size].filename, filename);
+        tmp[size].file_size = file_size;
+
+        size++;
     }
 
+    *files_size = size;
     *files = tmp;
     return 1;
 }
-
 
 int main(int argc, char * argv[])
 {
@@ -91,13 +136,21 @@ int main(int argc, char * argv[])
 
     int files_size = 0;
     File_data * files = NULL;
+
     if (!parse_files_data(&files,&files_size, argv, argc))
-    {
         return 1;
+    
+
+    for (int i = 0; i < files_size; i++)
+    {
+        printf("filepath %s\n", files[i].filepath);
+        printf("filename %s\n", files[i].filename);
+        printf("file size: %" PRIdMAX "\n", (intmax_t)files[i].file_size);
+        printf("\n");
     }
 
-    free(files);
-    files = NULL;
 
+
+    free_files(&files);
     return 0;
 }
